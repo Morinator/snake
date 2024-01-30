3c5b1c5303e12f860a0ccac829852ab4776a68d3
Moritz Groß <hi@mgross.dev>
2024-01-30 17:38:09 +0100

    move WIDTH and HEIGHT to constants

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,22 @@ use crate::draw::to_coord_u32;
 use crate::game::Game;
 
 const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+const WIDTH: i32 = 15;
+const HEIGHT: i32 = WIDTH;
 
 fn main() {
-    let (width, height) = (15, 15);
-
     let mut piston_window: PistonWindow =
-        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
+        WindowSettings::new("Snake", [to_coord_u32(WIDTH), to_coord_u32(HEIGHT)])
             .exit_on_esc(true)
             .build()
             .unwrap();
 
-    let mut game: Game = Game::new(width, height);
+    let mut game: Game = Game::new(WIDTH, HEIGHT);
     while let Some(event) = piston_window.next() {
         if let Some(Button::Keyboard(key)) = event.press_args() {
             game.key_pressed(key);
         }
-        piston_window.draw_2d(&event, 
-            |c, g, _| {
+        piston_window.draw_2d(&event, |c, g, _| {
             clear(BACK_COLOR, g);
             game.draw(&c, g);
         });
