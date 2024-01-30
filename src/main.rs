d028b75674d061f03e03efa42c65b2431f265f7b
Moritz Groß <hi@mgross.dev>
2024-01-30 18:37:34 +0100

    refactor direction array

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,17 @@ use piston_window::{PressEvent, UpdateEvent};
 const BACK_COLOR: pw::types::Color = [0.5, 0.5, 0.5, 1.0];
 const WIDTH: i32 = 15;
 const HEIGHT: i32 = WIDTH;
+const GAME_TITLE: &str = "Snake";
 
 fn main() {
-    let mut piston_window: pw::PistonWindow =
-        pw::WindowSettings::new("Snake", [to_coord_u32(WIDTH), to_coord_u32(HEIGHT)])
-            .exit_on_esc(true)
-            .build()
-            .unwrap();
-
+    let mut piston_window: pw::PistonWindow = pw_from_constants();
     let mut game: Game = Game::new(WIDTH, HEIGHT);
+
     while let Some(event) = piston_window.next() {
         if let Some(pw::Button::Keyboard(key)) = event.press_args() {
             game.key_pressed(key);
         }
+
         piston_window.draw_2d(&event, |c, g, _| {
             pw::clear(BACK_COLOR, g);
             game.draw(&c, g);
@@ -36,4 +34,13 @@ fn main() {
             game.update(arg.dt);
         });
     }
+}
+
+fn pw_from_constants() -> pw::PistonWindow {
+    let mut piston_window: pw::PistonWindow =
+        pw::WindowSettings::new(GAME_TITLE, [to_coord_u32(WIDTH), to_coord_u32(HEIGHT)])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
+    piston_window
 }
