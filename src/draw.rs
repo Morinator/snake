11efcd8eb8d96d69a5291e165d4bfc59f88809ca
Moritz Groß <hi@mgross.dev>
2024-01-30 18:23:25 +0100

    refactor draw.rs

--- src/draw.rs
+++ src/draw.rs
@@ -10,18 +10,6 @@ pub fn to_coord_u32(game_coord: i32) -> u32 {
     to_coord(game_coord) as u32
 }
 
-pub fn draw_block(color: pw::types::Color, x: i32, y: i32, con: &pw::Context, g: &mut pw::G2d) {
-    let gui_x = to_coord(x);
-    let gui_y = to_coord(y);
-
-    pw::rectangle(
-        color,
-        [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
-        con.transform,
-        g,
-    );
-}
-
 pub fn draw_rectangle(
     color: pw::types::Color,
     x: i32,
@@ -31,18 +19,19 @@ pub fn draw_rectangle(
     con: &pw::Context,
     g: &mut pw::G2d,
 ) {
-    let x = to_coord(x);
-    let y = to_coord(y);
-
     pw::rectangle(
         color,
         [
-            x,
-            y,
+            to_coord(x),
+            to_coord(y),
             BLOCK_SIZE * (width as f64),
             BLOCK_SIZE * (height as f64),
         ],
         con.transform,
         g,
-    );
+    )
+}
+
+pub fn draw_block(color: pw::types::Color, x: i32, y: i32, con: &pw::Context, g: &mut pw::G2d) {
+    draw_rectangle(color, x, y, 1, 1, con, g);
 }
